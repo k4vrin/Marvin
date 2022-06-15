651a055e4e386ecda9c548df5311e7ba2e352e35
Mostafa Hosseini <kavrindev@gmail.com>
2022-06-15 16:48:25 +0430

    Initial commit

---
+++ app/src/main/java/com/kavrin/marvin/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kavrin.marvin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kavrin.marvin.ui.theme.MarvinTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+		}
+	}
+}
