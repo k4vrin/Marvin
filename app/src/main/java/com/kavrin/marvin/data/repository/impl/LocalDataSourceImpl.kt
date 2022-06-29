aa5df33a13b4b3e858c8ddf28f7feb6a6de41f00
Mostafa Hosseini <kavrindev@gmail.com>
2022-06-29 21:40:23 +0430

    Add LocalDataSource

---
+++ app/src/main/java/com/kavrin/marvin/data/repository/impl/LocalDataSourceImpl.kt
@@ -0,0 +1,35 @@
+package com.kavrin.marvin.data.repository.impl
+
+import com.kavrin.marvin.data.local.MarvinDatabase
+import com.kavrin.marvin.domain.model.movie.entities.Movie
+import com.kavrin.marvin.domain.model.tv.entities.Tv
+import com.kavrin.marvin.domain.repository.LocalDataSource
+
+class LocalDataSourceImpl(
+    private val marvinDatabase: MarvinDatabase
+) : LocalDataSource {
+
+    override suspend fun getMovie(movieId: Int): Movie {
+        return marvinDatabase.movieDao().getSelectedMovie(movieId = movieId)
+    }
+
+    override suspend fun getTv(tvId: Int): Tv {
+        return marvinDatabase.tvDao().getSelectedTv(tvId = tvId)
+    }
+
+    override suspend fun getTvGenres(ids: List<Int>): List<String> {
+        val list = mutableListOf<String>()
+        ids.forEach { id ->
+            list.add(marvinDatabase.genreDao().getTvGenre(id = id))
+        }
+        return list
+    }
+
+    override suspend fun getMovieGenres(ids: List<Int>): List<String> {
+        val list = mutableListOf<String>()
+        ids.forEach { id ->
+            list.add(marvinDatabase.genreDao().getMovieGenre(id = id))
+        }
+        return list
+    }
+}
---
+++ app/src/main/java/com/kavrin/marvin/domain/repository/LocalDataSource.kt
@@ -0,0 +1,12 @@
+package com.kavrin.marvin.domain.repository
+
+import com.kavrin.marvin.domain.model.movie.entities.Movie
+import com.kavrin.marvin.domain.model.tv.entities.Tv
+
+interface LocalDataSource {
+
+    suspend fun getMovie(movieId: Int): Movie
+    suspend fun getTv(tvId: Int): Tv
+    suspend fun getTvGenres(ids: List<Int>): List<String>
+    suspend fun getMovieGenres(ids: List<Int>): List<String>
+}
