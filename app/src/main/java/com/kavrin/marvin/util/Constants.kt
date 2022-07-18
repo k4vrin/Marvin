6f3e18eb62e6521a416493c60901e525b79e0798
Mostafa Hosseini <kavrindev@gmail.com>
2022-07-18 21:10:03 +0430

    Update Gradle * Update Gradle to 7.5-rc-2 * Add READ permission in AndroidManifest.xml * Change poster base url * Add RecomposeHighlighter from https://github.com/android/snippets

--- app/src/main/java/com/kavrin/marvin/util/Constants.kt
+++ app/src/main/java/com/kavrin/marvin/util/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
 	const val BASE_URL = "https://api.themoviedb.org/3/"
 	const val IMAGE_BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w780/"
-	const val IMAGE_POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342/"
+	const val IMAGE_POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500/"
 	const val IMAGE_PROFILE_BASE_URL = "https://image.tmdb.org/t/p/w185/"
 	const val IMDb_BASE_URL = "https://imdb-api.com/en/API/"
 
